409f674691b8ad0189fc4bc373b47e1eb72d739e
Jefferson Ramos da Rocha <jeff.rocha3@gmail.com>
2019-10-14 21:30:34 -0300

    Commit inicial.

---
+++ src/main.ts
@@ -0,0 +1,21 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  // Open API
+  const options = new DocumentBuilder()
+    .setTitle('Gidu API')
+    .setDescription('API - Gestão de Igrejas Digitais Unificada')
+    .setVersion('1.0.0')
+    .addTag('gidu')
+    .build();
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('', app, document);
+
+  app.enableCors();
+  await app.listen(process.env.PORT || 3000);
+}
+bootstrap();
